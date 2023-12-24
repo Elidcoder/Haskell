ba12599702ac0364c10f19e03b9fd4ba40d5794e
Eli Gilbert <eliaarongilbert@Gmail.com>
2023-12-24 21:44:28 +0000

    part 2 SAT Solving finished

--- Haskell finals past papers/SAT Solving 2019/Skeleton.hs
+++ Haskell finals past papers/SAT Solving 2019/Skeleton.hs
@@ -6,6 +6,15 @@ import Data.Maybe
 import Types
 import TestData
 
+
+myFunc :: [[Int]] -> [[Int]]
+myFunc = filter isSingle 
+  where
+    isSingle (a: []) = True
+    isSingle _       = False
+
+
+    
 printF :: Formula -> IO()
 printF
   = putStrLn . showF
@@ -25,18 +34,23 @@ printF
 -- 1 mark
 lookUp :: Eq a => a -> [(a, b)] -> b
 -- Pre: The item being looked up has a unique binding in the list
-lookUp 
-  = undefined
+lookUp item list = b'
+  where 
+    ((a', b'): xs) = dropWhile ((/= item) . fst) list
 
 -- 3 marks
 vars :: Formula -> [Id]
-vars 
-  = undefined
+vars = sort . nub . vars'
+  where
+    vars' (And f1 f2) = vars' f1 ++ vars' f2
+    vars' (Or f1 f2)  = vars' f1 ++ vars' f2
+    vars' (Not f1)    = vars' f1 
+    vars' (Var id)    = [id]
 
 -- 1 mark
 idMap :: Formula -> IdMap
-idMap 
-  = undefined
+idMap = (`zip` [1..]) . vars  
+
 
 --------------------------------------------------------------------------
 -- Part II
@@ -45,27 +59,37 @@ idMap
 -- Both arguments are assumed to be in CNF, so the
 -- arguments of all And nodes will also be in CNF.
 distribute :: CNF -> CNF -> CNF
-distribute a (And b c)
-  = And (distribute a b) (distribute a c)
-distribute (And a b) c
-  = And (distribute a c) (distribute b c)
-distribute a b
-  = Or a b
+distribute a (And b c) = And (distribute a b) (distribute a c)
+distribute (And a b) c = And (distribute a c) (distribute b c)
+distribute a b         = Or a b
 
 -- 4 marks
 toNNF :: Formula -> NNF
-toNNF 
-  = undefined
-
+toNNF (Not (Not formula)) = toNNF formula
+toNNF (Not (And f1 f2))   = Or (toNNF (Not f1)) (toNNF (Not f2))
+toNNF (Not (Or f1 f2))    = And (toNNF (Not f1)) (toNNF (Not f2))
+toNNF (And f1 f2)         = And (toNNF f1) (toNNF f2)
+toNNF (Or f1 f2)          = Or (toNNF f1) (toNNF f2)
+toNNF (Not f1)            = Not (toNNF f1)
+toNNF f                   = f
 -- 3 marks
 toCNF :: Formula -> CNF
-toCNF 
-  = undefined
+toCNF = toCNF' . toNNF
+  where 
+    toCNF' :: Formula -> CNF
+    toCNF' (Or f1 f2) = distribute f1 f2
+    toCNF' f          = f
 
 -- 4 marks
 flatten :: CNF -> CNFRep
-flatten 
-  = undefined
+flatten formula = (map flatten' . unPackA) formula
+  where 
+    myIdMap = idMap formula
+    unPackA (And f1 f2) = (unPackA f1) ++ (unPackA f2)
+    unPackA f = [f]
+    flatten' (Or a b)  = (flatten' a) ++ (flatten' b)
+    flatten' (Not (Var id)) = [ - (lookUp id myIdMap)]
+    flatten' (Var id)  = [lookUp id myIdMap]
 
 --------------------------------------------------------------------------
 -- Part III
