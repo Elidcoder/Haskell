fa3abb70d691557389a7423312d45def890f32ce
Eli Gilbert <eliaarongilbert@Gmail.com>
2024-01-02 00:56:33 +0000

    completed up to Part 3 in 2021 paper

--- Haskell finals past papers/Graph colouring and register allocation 2021/Alloc.hs
+++ Haskell finals past papers/Graph colouring and register allocation 2021/Alloc.hs
@@ -11,52 +11,76 @@ import Examples
 -- Part I
 --
 count :: Eq a => a -> [a] -> Int
-count 
-  = undefined
+count val = foldl (flip ((+) . (fromEnum . (== val)))) 0 
 
 degrees :: Eq a => Graph a -> [(a, Int)]
-degrees 
-  = undefined
+degrees (values, edges)
+  = map (\x -> (x, length (filter (\(a,b) -> a == x || b == x ) edges))) values
 
 neighbours :: Eq a => a -> Graph a -> [a]
-neighbours 
-  = undefined
+neighbours val (values, edges)
+  = [if (a == val) then (b) else (a) | (a, b) <- edges, let a' = (a == val), a' || (b == val)]
 
 removeNode :: Eq a => a -> Graph a -> Graph a
-removeNode 
-  = undefined
+removeNode val (values, edges)
+  = (delete val values, filter (\(a, b) -> (a /= val) && (b /= val)) edges)
 
 ------------------------------------------------------
 --
 -- Part II
 --
 colourGraph :: (Ord a, Show a) => Int -> Graph a -> Colouring a
-colourGraph 
-  = undefined
-
+colourGraph maxColour ([],_)       = []
+colourGraph maxColour (values, []) = map (\x -> (x, 1)) values
+colourGraph maxColour curGraph@(values, edges) 
+  | null availableColours = (curNode, 0): nextColored
+  | otherwise, (minColour: colours) <- availableColours = (curNode, minColour): nextColored
+    where
+      (curNode: nodes) = sort values 
+      nextColored = colourGraph maxColour (removeNode curNode curGraph)
+      curNeighbours = neighbours curNode curGraph
+      availableColours = [1..maxColour] \\ [ b | (a,b) <- nextColored, a `elem` curNeighbours]
 ------------------------------------------------------
 --
 -- Part III
 --
 buildIdMap :: Colouring Id -> IdMap
-buildIdMap 
-  = undefined
+buildIdMap list
+  = ("return", "return"): map buildIdMap' list
+  where 
+    buildIdMap' (a, 0) = (a, a)
+    buildIdMap' (a, b) = (a, "R" ++ show b)
 
 buildArgAssignments :: [Id] -> IdMap -> [Statement]
-buildArgAssignments 
-  = undefined
+buildArgAssignments vars iMap = map (\x -> Assign (lookUp x iMap) (Var x)) vars
 
 renameExp :: Exp -> IdMap -> Exp
 -- Pre: A precondition is that every variable referenced in 
 -- the expression is in the idMap. 
-renameExp 
-  = undefined
+renameExp initialExpr idMap = renameExp' initialExpr
+  where
+    renameExp' (Var x)         = Var (lookUp x idMap)
+    renameExp' (Apply op exp1 exp2) = Apply op (renameExp' exp1) (renameExp' exp2)
+    renameExp' otherExpr          = otherExpr
 
 renameBlock :: Block -> IdMap -> Block
 -- Pre: A precondition is that every variable referenced in 
 -- the block is in the idMap. 
-renameBlock 
-  = undefined
+renameBlock inputBlock idMap = map renameStatement inputBlock
+  where
+    renameStatement :: Statement -> Statement
+    renameStatement (Assign id exp) = Assign lhs rhs
+        where
+            lhs = lookUp id idMap
+            rhs = renameExp exp idMap
+    renameStatement (If exp block1 block2) = If (renameExp exp idMap) (parseSubblock block1) (parseSubblock block2)
+    renameStatement (While exp block) = While (renameExp exp idMap) (parseSubblock block)
+    
+    parseSubblock :: Block -> Block
+    parseSubblock block = [renamed | x <- block, let renamed = renameStatement x, isGoodAssign renamed]
+
+    isGoodAssign (Assign id (Var exp')) = not (id == exp')
+    isGoodAssign _ = True
 
 renameFun :: Function -> IdMap -> Function
 renameFun (f, as, b) idMap
